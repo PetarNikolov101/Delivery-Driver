78a928ab45b46599b2f3a8ff0289e02d2b70a15c
PetarNikolov101 <petarnikolov101@gmail.com>
2024-02-08 15:26:12 +0100

    Serialized variables for steer speed and move speed

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
+    [SerializeField]float steerSpeed = 0.2f;
+    [SerializeField]float moveSpeed = 0.01f;
     // Start is called before the first frame update
     void Start()
     {
@@ -12,8 +14,8 @@ public class Driver : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {
-        transform.Rotate(0, 0, 0.1f);
-        transform.Translate(0,0.01f,0);
+    {        
+        transform.Rotate(0, 0, steerSpeed);
+        transform.Translate(0, moveSpeed, 0);
     }
 }
