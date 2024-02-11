9195b1532564e8fe669cfb31e147d391bfd006ae
PetarNikolov101 <petarnikolov101@gmail.com>
2024-02-11 10:42:47 +0100

    added time so that steering and moving works the same at all fps speeds Added a circle and collision between it and the car

--- Assets/Driver.cs
+++ Assets/Driver.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class Driver : MonoBehaviour
 {
-    [SerializeField]float steerSpeed = 0.2f;
-    [SerializeField]float moveSpeed = 0.01f;
+    [SerializeField]float steerSpeed = 250f;
+    [SerializeField]float moveSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,8 +14,10 @@ public class Driver : MonoBehaviour
 
     // Update is called once per frame
     void Update()
-    {        
-        transform.Rotate(0, 0, steerSpeed);
-        transform.Translate(0, moveSpeed, 0);
+    {
+        float steerAmount = Input.GetAxis("Horizontal") * steerSpeed * Time.deltaTime;
+        float moveAmount = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;        
+        transform.Rotate(0, 0, -steerAmount);
+        transform.Translate(0, moveAmount, 0);
     }
 }
