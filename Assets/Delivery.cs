61776eb26c5a27bdebcbe744816c5a35a43d257f
PetarNikolov101 <petarnikolov101@gmail.com>
2024-02-16 19:58:07 +0100

    made package and customer triggers

--- Assets/Delivery.cs
+++ Assets/Delivery.cs
@@ -4,12 +4,17 @@ using UnityEngine;
 
 public class Delivery : MonoBehaviour
 {
-    void OnCollisionEnter2D(){
+    void OnCollisionEnter2D(Collision2D other){
         Debug.Log("bump");
     }
 
-    void OnTriggerEnter2D() {
-        Debug.Log("Trigger!");
+    void OnTriggerEnter2D(Collider2D other) {
+        if(other.tag == "Package"){
+            Debug.Log("Package picked up");
+        }
+        if(other.tag == "Customer"){
+            Debug.Log("Delivered Package");
+        }
     }
 }
 
